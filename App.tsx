f88fef071d472bfcdd8a280f1f89bd4d1cb6e107
LucasAlencar <lucas.linard@outlook.com>
2023-05-01 11:14:21 -0300

    feat: NativeBaseProvider & Karla custom fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SignIn } from "@screens/SignIn";
+import { StatusBar } from "expo-status-bar";
+import { NativeBaseProvider } from "native-base";
+import { useFonts, Karla_400Regular, Karla_700Bold } from "@expo-google-fonts/karla";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <StatusBar style="dark" backgroundColor="transparent" translucent />
+      { fontsLoaded ? <SignIn />: <Loading/>}
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,15 @@
+import { VStack,Center , Text } from "native-base";
+
+
+export function SignIn(){
+    return(
+        <Center flex={1}>
+            <VStack space={4}>
+                <Text>Sign In</Text>
+            </VStack>
+        </Center>
+    )
+}
+
+
+
