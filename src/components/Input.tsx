6a11d8ac51881af3e4a3386208083e2c74f51450
LucasAlencar <lucas.linard@outlook.com>
2023-05-01 16:14:13 -0300

    feat: Text Input components

---
+++ src/components/Input.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import {
+  Input as NativeBaseInput,
+  IInputProps,
+  Text,
+  useTheme,
+  Pressable,
+  Box,
+  Divider,
+  Center,
+} from "native-base";
+
+import {
+  Eye,
+  EyeClosed,
+  MagnifyingGlass,
+  Sliders,
+} from "phosphor-react-native";
+
+type InputVariant = "default" | "password" | "search" | "cash";
+
+type InputProps = IInputProps & {
+  variant?: InputVariant;
+};
+
+export function Input({ variant = "default", ...rest }: InputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+  const { colors } = useTheme();
+
+  function renderPasswordVariant() {
+    return (
+      <Pressable mr="4" onPress={() => setShowPassword(!showPassword)}>
+        {showPassword ? (
+          <Eye size={24} color={colors.gray[500]} />
+        ) : (
+          <EyeClosed size={24} color={colors.gray[500]} />
+        )}
+      </Pressable>
+    );
+  }
+
+  function renderSearchVariant() {
+    return (
+      <Center flexDirection="row" height="full">
+        <MagnifyingGlass size={24} color={colors.gray[500]} />
+        <Box py="3">
+          <Divider orientation="vertical" mx="3" my="1.5" thickness="2" />
+        </Box>
+        <Pressable mr="4" onPress={() => {}} _pressed={{ opacity: "0.5" }}>
+          <Sliders size={24} color={colors.gray[500]} />
+        </Pressable>
+      </Center>
+    );
+  }
+
+  return (
+    <NativeBaseInput
+      h="12"
+      px="4"
+      py="3"
+      mb="4"
+      secureTextEntry={variant === "password" && !showPassword}
+      borderRadius="1.5"
+      borderWidth="0"
+      fontFamily={"body"}
+      fontSize="md"
+      bgColor="white"
+      placeholderTextColor={"gray.400"}
+      color={"gray.600"}
+      _focus={{
+        borderWidth: "1",
+        borderColor: "gray.500",
+      }}
+      InputLeftElement={
+        variant === "cash" ? (
+          <Text ml="4" mr="-2" color="gray.700">
+            R$
+          </Text>
+        ) : undefined
+      }
+      InputRightElement={
+        (variant === "password" && renderPasswordVariant()) ||
+        (variant === "search" && renderSearchVariant()) ||
+        undefined
+      }
+      {...rest}
+    />
+  );
+}
---
+++ src/components/MultilineInput.tsx
@@ -0,0 +1,30 @@
+import {
+  TextArea,
+  ITextAreaProps,
+} from "native-base";
+
+
+export function MultilineInput({ ...rest }: ITextAreaProps) {
+  return (
+    <TextArea
+    totalLines={8}
+    autoCompleteType={"off"}
+      h="40"
+      px="4"
+      py="3"
+      mb="4"
+      borderRadius="1.5"
+      borderWidth="0"
+      fontFamily={"body"}
+      fontSize="md"
+      bgColor="white"
+      color={"gray.600"}
+      placeholderTextColor={"gray.400"}
+      _focus={{
+        borderWidth: "1",
+        borderColor: "gray.500",
+      }}
+      {...rest}
+    />
+  );
+}
