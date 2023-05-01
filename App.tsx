f8629c86d9b96bdc08ac7593dd3bf7cb7ea2e584
LucasAlencar <lucas.linard@outlook.com>
2023-05-01 12:57:51 -0300

    feat: custom theme

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
 import { useFonts, Karla_400Regular, Karla_700Bold } from "@expo-google-fonts/karla";
 import { Loading } from "@components/Loading";
-
+import { theme } from './src/theme'
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
 
+
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <StatusBar style="dark" backgroundColor="transparent" translucent />
       { fontsLoaded ? <SignIn />: <Loading/>}
     </NativeBaseProvider>
