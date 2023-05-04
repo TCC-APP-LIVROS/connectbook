a64ed29869b42bdb5fe674bf2f208f6884c4dc1f
LucasAlencar <lucas.linard@outlook.com>
2023-05-04 15:27:27 -0300

    refactor: SignIn now uses i18n

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -9,35 +9,36 @@ import { useTranslation } from "react-i18next";
 export function SignIn() {
   const { t, i18n } = useTranslation();
   return (
+    // adicionar em uma scroll view para devices menores?
     <VStack flex={1}>
       <VStack px={10} borderBottomRadius={"12"} bg={"gray.200"}>
-        <Center w="full" mt="16">
-          <Image source={Logo} alt="Logo" mb="5" mt={10} />
+        <Center w="full" mt="24">
+          <Image source={Logo} alt="Logo" mb="5" />
           <Heading color="gray.700" fontSize="3.5xl" fontFamily="heading">
             marketspace
           </Heading>
           <Text color="gray.500" fontSize="sm">
-            {t("Your buying and selling space")}
+            {t("SignIn:Slogan")}
           </Text>
         </Center>
 
         <Center>
           <Text mb="4" mt="20" fontSize="sm">
-            {t("login to your account")}
+            {t("SignIn:login-to-your-account")}
           </Text>
-          <Input placeholder={t("E-mail")} />
-          <Input placeholder={t("Password")} variant="password" />
-          <Button w="full" type="secondary" mt="4" title="Entrar" mb="16" />
+          <Input placeholder={t("Common:E-mail")} />
+          <Input placeholder={t("Common:Password")} variant="password" />
+          <Button w="full" type="secondary" mt="4" title={t("SignIn:SignIn")} mb="16" />
         </Center>
       </VStack>
 
       <VStack flex={1} px={10} justifyContent={"center"} bg="gray.100">
         <Center>
           <Text color="gray.500" fontSize="sm">
-            {t("Don't have an account?")}
+            {t("SignIn:Dont-have-an-account")}
           </Text>
           <Button
-            title={t("Create account")}
+            title={t("SignIn:Create-account")}
             type="tertiary"
             w="full"
             mt="4"
