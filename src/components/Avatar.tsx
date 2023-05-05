d8c6fa2d27d428748d040deaf2285cb8941a9cea
LucasAlencar <lucas.linard@outlook.com>
2023-05-05 14:04:48 -0300

    refactor: updated rendering logic to always show badge when 'badge' prop is provided

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,16 +3,23 @@ import { Avatar as NativeBaseAvatar, IAvatarProps, Center } from "native-base";
 import DefaultAvatar from "@assets/Img/DefaultAvatar/Avatar.png";
 import { PencilLine, PencilSimpleLine } from "phosphor-react-native";
 
-export function Avatar({ source, ...rest }: IAvatarProps) {
+type AvatarProps = IAvatarProps & {
+  badge?: boolean;
+};
+
+export function Avatar({
+  source,
+  badge = false,
+  ...rest
+}: AvatarProps) {
   return (
     <NativeBaseAvatar
       borderWidth="1.5"
       borderColor={"blue.600"}
       source={source || DefaultAvatar}
-      size="22"
       {...rest}
     >
-      {source ? null : (
+      {badge ? (
         <NativeBaseAvatar.Badge
           bg={"blue.600"}
           size="10"
@@ -21,7 +28,7 @@ export function Avatar({ source, ...rest }: IAvatarProps) {
         >
           <PencilSimpleLine size="16" color={"white"} />
         </NativeBaseAvatar.Badge>
-      )}
+      ) : null}
     </NativeBaseAvatar>
   );
 }
