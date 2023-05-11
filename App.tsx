f2e0dddc5ce417df9a756d6f109d40fadf66f392
LucasAlencar <lucas.linard@outlook.com>
2023-05-11 17:19:53 -0300

    refactor: App.tsx render routes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import { SignIn } from "@screens/SignIn";
 import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
 import { useFonts, Karla_400Regular, Karla_700Bold } from "@expo-google-fonts/karla";
 import { Loading } from "@components/Loading";
+import { Routes } from "./src/routes";
+
 import { theme } from './src/theme'
 
 import "./src/i18n"
+import "react-native-gesture-handler"
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
@@ -14,7 +16,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
       <StatusBar style="dark" backgroundColor="transparent" translucent />
-      { fontsLoaded ? <SignIn />: <Loading/>}
+      { fontsLoaded ? <Routes />: <Loading/>}
     </NativeBaseProvider>
   );
 }
