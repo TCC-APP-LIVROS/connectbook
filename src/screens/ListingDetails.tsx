7a4db153d41417b2f9b5c920e0eb537cf7c45ef0
LucasAlencar <lucas.linard@outlook.com>
2023-05-11 14:51:01 -0300

    feat: ListingDetails dealer specific interface

--- src/screens/ListingDetails.tsx
+++ src/screens/ListingDetails.tsx
@@ -11,6 +11,8 @@ import {
 } from "native-base";
 import {
   Pencil,
+  Power,
+  TrashSimple,
   WhatsappLogo,
 } from "phosphor-react-native";
 import { Avatar } from "@components/Avatar";
@@ -22,21 +24,52 @@ import { Platform } from "react-native";
 
 export function ListingDetails() {
   const { colors } = useTheme();
+  const route = {
+    listingType: "user", // user or dealer
+    isListingActive: false, // active or inactive
+  };
   return (
     <VStack flex={1} safeAreaX safeAreaTop>
       <Box px="6">
         <Header
           backButton
-          title="Listing Details"
-          rightButtonIcon={<Pencil size={24} />}
+          rightButtonIcon={
+            route.listingType === "user" ? <Pencil size={24} /> : undefined
+          }
         />
       </Box>
-      <Carrousel mt={3} />
+      <VStack>
+        <Carrousel />
+        {route.isListingActive ? null : (
+          <>
+            <Box position="absolute" w="full" height="full">
+              <Box
+                position="absolute"
+                w="full"
+                height="full"
+                bg="gray.700"
+                opacity="0.6"
+                justifyContent={"center"}
+                alignItems={"center"}
+              />
+              <Heading
+                color="gray.100"
+                fontSize="xs"
+                fontFamily="heading"
+                textAlign={"center"}
+                textTransform={"uppercase"}
+                top={"50%"}
+              >
+                Anúncio desativado
+              </Heading>
+            </Box>
+          </>
+        )}
+      </VStack>
 
       <ScrollView
         flex="1"
         px="6"
-        mt={2}
         pt={3}
         pb={10}
         showsVerticalScrollIndicator={false}
@@ -52,7 +85,12 @@ export function ListingDetails() {
           <Tag mt={5} title="novo" bgColor={"gray.300"} titleColor="gray.600" />
         </HStack>
 
-        <VStack mt={5} mb={2} alignItems="flex-start" justifyContent="space-between">
+        <VStack
+          mt={5}
+          mb={2}
+          alignItems="flex-start"
+          justifyContent="space-between"
+        >
           <Heading fontSize="xl" fontFamily="heading" color="gray.700">
             Bicicleta Muito ultra mega blaster ultra
           </Heading>
@@ -93,36 +131,62 @@ export function ListingDetails() {
         <PaymentMethod payment="Dinheiro" color={colors.gray[700]} />
         <PaymentMethod payment="Cartão" color={colors.gray[700]} />
         <PaymentMethod payment="Depósito Bancário" color={colors.gray[700]} />
+        {route.listingType === "user" && (
+          <>
+            {route.isListingActive ? (
+              <Button
+                title="Desativar anúncio"
+                mt="8"
+                startIcon={<Power size={16} color={colors.gray[100]} />}
+              />
+            ) : (
+              <Button
+                title="Reativar anúncio"
+                type="secondary"
+                mt="8"
+                startIcon={<Power size={16} color={colors.gray[100]} />}
+              />
+            )}
+            <Button
+              title="Excluir anúncio"
+              type="tertiary"
+              mt="2"
+              startIcon={<TrashSimple size={16} color={colors.gray[400]} />}
+            />
+          </>
+        )}
         <Box h="10" />
       </ScrollView>
-      <HStack
-        w="full"
-        bg="white"
-        justifyContent="space-between"
-        alignItems={"center"}
-        paddingTop={5}
-        paddingBottom={Platform.OS === "ios" ? 7 : 5}
-        paddingX={6}
-      >
-        <Heading
-          fontSize="sm"
-          fontFamily="heading"
-          color="blue.800"
-          lineHeight="xl"
+      {route.listingType === "dealer" && (
+        <HStack
+          w="full"
+          bg="white"
+          justifyContent="space-between"
+          alignItems={"center"}
+          paddingTop={5}
+          paddingBottom={Platform.OS === "ios" ? 7 : 5}
+          paddingX={6}
         >
-          R${" "}
-          <Heading fontSize="xl" fontFamily="heading" color="blue.800">
-            120,00
+          <Heading
+            fontSize="sm"
+            fontFamily="heading"
+            color="blue.800"
+            lineHeight="xl"
+          >
+            R${" "}
+            <Heading fontSize="xl" fontFamily="heading" color="blue.800">
+              120,00
+            </Heading>
           </Heading>
-        </Heading>
-        <Button
-          type="secondary"
-          startIcon={
-            <WhatsappLogo size={24} weight="fill" color={colors.white} />
-          }
-          title="Entrar em contato"
-        />
-      </HStack>
+          <Button
+            type="secondary"
+            startIcon={
+              <WhatsappLogo size={24} weight="fill" color={colors.white} />
+            }
+            title="Entrar em contato"
+          />
+        </HStack>
+      )}
     </VStack>
   );
 }
