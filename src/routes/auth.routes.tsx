f34ce92d132409ed2e299907261ae01b282507f6
LucasAlencar <lucas.linard@outlook.com>
2023-05-11 16:02:09 -0300

    refactor: AuthRoutes type

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,11 +1,16 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeStackNavigationProp, createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { SignIn } from "@screens/SignIn";
 import { SignUp } from "@screens/SignUp";
 
+type AuthRoutes = {
+  SignIn: undefined;
+  SignUp: undefined;
+}
+
+export type AuthNavigationRouteProps = NativeStackNavigationProp<AuthRoutes>; 
 
 const { Navigator, Screen } = createNativeStackNavigator();
-
 export function AuthRoutes() {
   return (
     <Navigator screenOptions={{headerShown: false}}>
