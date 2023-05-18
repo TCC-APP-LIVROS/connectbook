0cacc4c2c843f1ffacaf200f09f264ba936350ee
LucasAlencar <lucas.linard@outlook.com>
2023-05-18 19:03:27 -0300

    feat: routes based if is logged in

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,21 @@
 import { Box } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
+import { useAuth } from "@hooks/useAuth";
+import { Loading } from "@components/Loading";
 
 export function Routes() {
+  const { user, isLoadingUserData } = useAuth();
+
+  if (isLoadingUserData) {
+    return <Loading />;
+  }
+
   return (
     <Box flex={1} bg="gray.100">
       <NavigationContainer>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
