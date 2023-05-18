8cd0762f5d702074383532caa274bb297493dea8
LucasAlencar <lucas.linard@outlook.com>
2023-05-18 19:02:32 -0300

    feat: AuthContextProvider on App.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
-import { useFonts, Karla_400Regular, Karla_700Bold } from "@expo-google-fonts/karla";
+import {
+  useFonts,
+  Karla_400Regular,
+  Karla_700Bold,
+} from "@expo-google-fonts/karla";
 import { Loading } from "@components/Loading";
 import { Routes } from "./src/routes";
 
-import { theme } from './src/theme'
+import { theme } from "./src/theme";
 
-import "./src/i18n"
-import "react-native-gesture-handler"
+import { AuthContextProvider } from "@contexts/AuthContext";
+import "./src/i18n";
+import "react-native-gesture-handler";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
-
-
   return (
     <NativeBaseProvider theme={theme}>
       <StatusBar style="dark" backgroundColor="transparent" translucent />
-      { fontsLoaded ? <Routes />: <Loading/>}
+      <AuthContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
