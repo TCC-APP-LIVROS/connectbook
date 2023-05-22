1e3badac0d0b8df0d7415f3bb7434d0e90342918
LucasAlencar <lucas.linard@outlook.com>
2023-05-22 19:11:55 -0300

    feat: authToken remove

--- src/storage/storageAuthToken.tsx
+++ src/storage/storageAuthToken.tsx
@@ -1,11 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AUTH_TOKEN_STORAGE } from "@storage/storageConfig";
 
 export async function storageAuthTokenSave(token: string, refresh_token: string) {
     const data = JSON.stringify({ token, refresh_token });
-    await AsyncStorage.setItem("@marketspace:authToken", data);
+    await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, data);
 }
 
-export async function storageAuthTokenGet(token: string, refresh_token: string) {
-    const data = JSON.stringify({ token, refresh_token });
-    await AsyncStorage.setItem("@marketspace:authToken", data);
+export async function storageAuthTokenGet() {
+    const storage = await AsyncStorage.getItem(AUTH_TOKEN_STORAGE);
+    const data = storage ? JSON.parse(storage) : null;
+    return data;
 }
+
+export async function storageAuthTokenRemove() {
+    await AsyncStorage.removeItem(AUTH_TOKEN_STORAGE);
+  }
