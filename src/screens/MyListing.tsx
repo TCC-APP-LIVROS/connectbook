21b90312877e91539e3e3cfe772252fa1b3ca79e
LucasAlencar <lucas.linard@outlook.com>
2023-05-30 20:20:31 -0300

    feat: Implemented API calls on MyListing screen

--- src/screens/MyListing.tsx
+++ src/screens/MyListing.tsx
@@ -1,13 +1,70 @@
-import { Header } from "@components/Header";
-import { HStack, VStack, Text, CheckIcon, FlatList } from "native-base";
+import { useCallback, useState } from "react";
+import { HStack, VStack, Text, FlatList, useToast } from "native-base";
 import { Plus } from "phosphor-react-native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+
+import { AppNavigationRouteProps } from "@routes/app.routes";
+import { AppError } from "@utils/AppError";
+import { api } from "@services/api";
+import { Header } from "@components/Header";
 import { Select } from "@components/Select";
 import { ProductCard } from "@components/ProductCard";
-
-import DefaultImage from "@assets/Img/Image.png";
+import { useAuth } from "@hooks/useAuth";
+import { Loading } from "@components/Loading";
 
 export function MyListing() {
   const countries = ["Todos", "Ativos", "Inativos"];
+  const [selectedFilter, setSelectedFilter] = useState(countries[0]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [listings, setListings] = useState<any[]>([]);
+  const { user } = useAuth();
+  const toast = useToast();
+  const navigation = useNavigation<AppNavigationRouteProps>();
+
+  const filteredProducts = listings.filter((item) => {
+    if (selectedFilter === "Todos") {
+      return true; // Retorna todos os produtos
+    } else if (selectedFilter === "Ativos") {
+      return item.is_active === true; // Retorna apenas produtos ativos
+    } else if (selectedFilter === "Inativos") {
+      return item.is_active === false; // Retorna apenas produtos inativos
+    }
+    return false; // Filtro inválido, não retorna nenhum produto
+  });
+
+  function handleGoToListingDetails() {
+    navigation.navigate("listingDetails");
+  }
+
+  async function fetchProducts() {
+    try {
+      setIsFetching(true);
+      const { data } = await api.get(`/users/products`);
+      setListings(data);
+    } catch (error) {
+      console.log(error);
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi Encontrar os produtos, tente novamente mais tarde";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "error.500",
+      });
+      setListings([]);
+    } finally {
+      setIsFetching(false);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchProducts();
+    }, [])
+  );
+
   return (
     <VStack flex={1} safeArea px="6" bg="gray.200">
       <Header title="My Listing" rightButtonIcon={<Plus size={24} />} />
@@ -17,35 +74,53 @@ export function MyListing() {
         <Select
           data={countries}
           defaultValueByIndex={0}
-          onSelect={(selectedItem, index) => {
-            console.log(selectedItem, index);
+          onSelect={(selectedItem) => {
+            setSelectedFilter(selectedItem);
           }}
         />
       </HStack>
       <VStack mt="4" flex={1}>
         {/* da pra melhorar o espaçamento e estilo */}
-        <FlatList
-          data={[1, 2, 3, 4, 5, 6, 7, 8]}
-          keyExtractor={(item) => String(item)}
-          numColumns={2}
-          _contentContainerStyle={{
-            justifyContent: "space-between",
-            alignItems: "center",
-            paddingBottom: 100,
-          }}
-          showsVerticalScrollIndicator={false}
-          renderItem={() => (
-            <ProductCard
-              mx={2}
-              mb={3}
-              image={DefaultImage}
-              avatarImage={{
-                uri: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg",
-              }}
-              title="Tênis Nike"
-            />
-          )}
-        />
+        {isFetching ? (
+          <Loading />
+        ) : (
+          <FlatList
+            data={filteredProducts}
+            keyExtractor={(item) => String(item)}
+            numColumns={2}
+            _contentContainerStyle={{
+              justifyContent: "space-between",
+              paddingBottom: 100,
+            }}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <ProductCard
+                mx={2}
+                mb={3}
+                image={{
+                  uri: `${api.defaults.baseURL}/images/${item.product_images[0].path}`,
+                }}
+                onPress={handleGoToListingDetails}
+                avatarImage={{
+                  uri: `${api.defaults.baseURL}/images/${user.avatar}`,
+                }}
+                title={item.name}
+                price={item.price}
+                isNew={item.is_new}
+                isActive={item.is_active}
+              />
+            )}
+            ListEmptyComponent={() => (
+              <VStack flex={1} justifyContent="center" alignItems="center">
+                <Text color="gray.500">
+                  {
+                    "Você não tem nenhum produto anunciado? \n que tal criar o seu primeiro?"
+                  }
+                </Text>
+              </VStack>
+            )}
+          />
+        )}
       </VStack>
     </VStack>
   );
