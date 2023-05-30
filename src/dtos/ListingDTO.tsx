d08dc7cd235b6993cbd1d78010faf77953b385ef
LucasAlencar <lucas.linard@outlook.com>
2023-05-30 17:25:26 -0300

    refactor: Added new optional properties for ListingDTO

--- src/dtos/ListingDTO.tsx
+++ src/dtos/ListingDTO.tsx
@@ -3,6 +3,8 @@ export type PaymentMethod = "pix" | "card" | "deposit" | "cash" | "boleto";
 export const paymentMethods: PaymentMethod[] = ["pix", "card", "deposit", "cash", "boleto"];
 
 export type ListingDTO = {
+    id? : string;
+    user_id?: string;
     name: string;
     description: string;
     is_new: boolean;
