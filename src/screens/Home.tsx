96f1e5d591a46c8ecd364db7bcb3af1ec7b4a71e
LucasAlencar <lucas.linard@outlook.com>
2023-05-30 18:51:17 -0300

    feat: Implemented API and product filtering at Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import {
   HStack,
   VStack,
@@ -9,33 +10,65 @@ import {
   Actionsheet,
   useDisclose,
   Heading,
-  Checkbox,
   Pressable,
+  useToast,
 } from "native-base";
+import { ArrowRight, Plus, Tag, X } from "phosphor-react-native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+
+import { AppNavigationRouteProps } from "@routes/app.routes";
+import { paymentMethods } from "@dtos/ListingDTO";
+import { useAuth } from "@hooks/useAuth";
+import { api } from "@services/api";
+import { AppError } from "@utils/AppError";
+
+import { Toggle } from "@components/Toggle";
+import { Switch } from "@components/Switch";
 import { Avatar } from "@components/Avatar";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ProductCard } from "@components/ProductCard";
-import { ArrowRight, Plus, Tag, X } from "phosphor-react-native";
+import { Checkbox } from "@components/Checkbox";
+import { Loading } from "@components/Loading";
 
-import DefaultImage from "@assets/Img/Image.png";
-import { Toggle } from "@components/Toggle";
-import { Switch } from "@components/Switch";
-import { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
-import { AppNavigationRouteProps } from "@routes/app.routes";
+type FilterOptions = {
+  is_new?: boolean;
+  accept_trade?: boolean;
+  payment_methods: string[];
+  query?: string;
+};
 
 export function Home() {
   const navigation = useNavigation<AppNavigationRouteProps>();
+  const { user } = useAuth();
   const { colors } = useTheme();
+  const toast = useToast();
   const { isOpen, onOpen, onClose } = useDisclose();
-  const [groupValues, setGroupValues] = useState([
-    "Boleto",
-    "Pix",
-    "Dinheiro",
-    "Cartão de Crédito",
-    "Depósito Bancário",
-  ]);
+  const [listings, setListings] = useState<any[]>([]);
+  const [userListings, setUserListings] = useState(0);
+  const [search, setSearch] = useState<string>("");
+  const [isFetching, setIsFetching] = useState(false);
+  const [filter, setFilter] = useState<FilterOptions>({
+    is_new: undefined,
+    accept_trade: undefined,
+    payment_methods: [],
+    query: undefined,
+  });
+
+  const PaymentOptions = paymentMethods.map((payment) => {
+    return { title: payment, value: payment };
+  });
+
+  function handleOnChangeFilter(change: Partial<FilterOptions>) {
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      ...change,
+    }));
+  }
+
+  function handleSearch() {
+    handleOnChangeFilter({ query: search });
+  }
 
   function handleGoToCreateListing() {
     navigation.navigate("createListing");
@@ -48,13 +81,118 @@ export function Home() {
     navigation.navigate("bottomTabsRoutes", { screen: "myListing" });
   }
 
+  function createQueryString(obj: any) {
+    const queryString = [];
+
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const value = obj[key];
+
+        if (value !== undefined) {
+          if (Array.isArray(value)) {
+            if (value.length > 0) {
+              value.forEach((item) =>
+                queryString.push(
+                  `${encodeURIComponent(key)}=${encodeURIComponent(item)}`
+                )
+              );
+            }
+          } else {
+            queryString.push(
+              `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+            );
+          }
+        }
+      }
+    }
+
+    return `?${queryString.join("&")}`;
+  }
+
+  async function fetchUserProducts() {
+    try {
+      const { data } = await api.get(`/users/products`);
+      setUserListings(data.length);
+    } catch (error) {}
+  }
+
+  async function fetchProducts() {
+    try {
+      setIsFetching(true);
+      const { data } = await api.get(`/products`);
+      setListings(data);
+    } catch (error) {
+      console.log(error);
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi Encontrar os produtos, tente novamente mais tarde";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "error.500",
+      });
+      setListings([]);
+    } finally {
+      setIsFetching(false);
+    }
+  }
+
+  async function fetchFilteredProducts(filters: FilterOptions) {
+    try {
+      setIsFetching(true);
+      const qs = createQueryString(filters);
+      const { data } = await api.get(`/products${qs}`);
+      setListings(data);
+    } catch (error) {
+      console.log(error);
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi Encontrar os produtos, tente novamente mais tarde";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+      setListings([]);
+    } finally {
+      setIsFetching(false);
+    }
+  }
+
+  async function handleFilterProducts() {
+    await fetchFilteredProducts(filter);
+    onClose();
+  }
+
+  async function handleClearFilter() {
+    setFilter({
+      is_new: undefined,
+      accept_trade: undefined,
+      payment_methods: [],
+      query: undefined,
+    });
+    await fetchProducts();
+  }
+  useFocusEffect(
+    useCallback(() => {
+      fetchUserProducts();
+      fetchProducts();
+    }, [])
+  );
   return (
     <VStack flex={1} px={6} bg="gray.200">
       <HStack mt="16">
-        <Avatar size="11.25" />
+        <Avatar
+          source={{ uri: `${api.defaults.baseURL}/images/${user.avatar}` }}
+          size="11.25"
+        />
         <VStack flex={1} ml="2.5">
           <Text>Boas vindas,</Text>
-          <Text fontFamily="heading">João</Text>
+          <Text fontFamily="heading">{user.name}</Text>
         </VStack>
         <Button
           startIcon={<Icon as={<Plus color="#fff" size="16" />} />}
@@ -79,7 +217,7 @@ export function Home() {
           </Box>
           <VStack ml="4" flex={1}>
             <Text color="gray.600" fontFamily="heading">
-              4
+              {userListings}
             </Text>
             <Text color="gray.600">Anúncios ativos</Text>
           </VStack>
@@ -102,31 +240,45 @@ export function Home() {
           placeholder="Pesquisar"
           variant="search"
           onFilterPress={onOpen}
+          onChangeText={setSearch}
+          onSearchPress={handleSearch}
         />
-        {/* da pra melhorar o espaçamento e estilo */}
-        <FlatList
-          data={[1, 2, 3, 4, 5, 6, 7, 8]}
-          keyExtractor={(item) => String(item)}
-          numColumns={2}
-          _contentContainerStyle={{
-            justifyContent: "space-between",
-            alignItems: "center",
-            paddingBottom: 100,
-          }}
-          showsVerticalScrollIndicator={false}
-          renderItem={() => (
-            <ProductCard
-              mx="2"
-              mb="6"
-              image={DefaultImage}
-              onPress={handleGoToListingDetails}
-              avatarImage={{
-                uri: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg",
-              }}
-              title="Tênis Nike"
-            />
-          )}
-        />
+        {isFetching ? (
+          <Loading />
+        ) : (
+          <FlatList
+            data={listings}
+            keyExtractor={(item) => item.id}
+            numColumns={2}
+            _contentContainerStyle={{
+              justifyContent: "space-between",
+              paddingBottom: 100,
+            }}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <ProductCard
+                mx="2"
+                mb="6"
+                image={{
+                  uri: `${api.defaults.baseURL}/images/${item.product_images[0].path}`,
+                }}
+                onPress={handleGoToListingDetails}
+                avatarImage={{
+                  uri: `${api.defaults.baseURL}/images/${item.user.avatar}`,
+                }}
+                title={item.name}
+                price={item.price}
+                isNew={item.is_new}
+              />
+            )}
+            refreshing={isFetching}
+            ListEmptyComponent={() => (
+              <VStack flex={1} justifyContent="center" alignItems="center">
+                <Text color="gray.500">Nenhum produto encontrado</Text>
+              </VStack>
+            )}
+          />
+        )}
       </VStack>
       <VStack>
         {/* eu quero dar um refactor e usar outro ao inves do actionsheet possivelmente */}
@@ -150,47 +302,66 @@ export function Home() {
                 Condição
               </Heading>
               <HStack mt="3">
-                <Toggle title="Novo" value={true} />
-                <Toggle title="Usado" value={false} ml="2" />
+                <Toggle
+                  title="Novo"
+                  onPress={() =>
+                    handleOnChangeFilter({
+                      is_new: filter.is_new == true ? undefined : true,
+                    })
+                  }
+                  value={filter.is_new == true}
+                />
+                <Toggle
+                  title="Usado"
+                  onPress={() =>
+                    handleOnChangeFilter({
+                      is_new: filter.is_new == false ? undefined : false,
+                    })
+                  }
+                  value={filter.is_new == false}
+                  ml="2"
+                />
               </HStack>
             </Box>
             <Box w="100%" mt="6">
               <Heading fontSize="sm" color="gray.600" fontFamily="heading">
                 Aceita troca?
               </Heading>
-              <Switch mt="3" />
+              <Switch
+                onToggle={() =>
+                  handleOnChangeFilter({
+                    accept_trade: !filter.accept_trade,
+                  })
+                }
+                value={filter.accept_trade}
+                mt="3"
+              />
             </Box>
             <Box w="100%" mt="6">
               <Heading fontSize="sm" color="gray.600" fontFamily="heading">
                 Meios de Pagamento
               </Heading>
-              <Checkbox.Group
-                onChange={setGroupValues}
-                value={groupValues}
-                accessibilityLabel="choose numbers"
-                mt="3"
-              >
-                <Checkbox value="Boleto" my="1" colorScheme="blue">
-                  Boleto
-                </Checkbox>
-                <Checkbox value="Pix" my="1" colorScheme="blue">
-                  Pix
-                </Checkbox>
-                <Checkbox value="Dinheiro" my="1" colorScheme="blue">
-                  Dinheiro
-                </Checkbox>
-                <Checkbox value="Cartão de Crédito" my="1" colorScheme="blue">
-                  Cartão de credito
-                </Checkbox>
-                <Checkbox value="Depósito Bancário" my="1" colorScheme="blue">
-                  Depósito Bancário
-                </Checkbox>
-              </Checkbox.Group>
+              <Checkbox
+                options={PaymentOptions}
+                value={filter.payment_methods}
+                onChange={(value) =>
+                  handleOnChangeFilter({ payment_methods: value })
+                }
+              />
             </Box>
             <Box w="100%" mt="6">
               <HStack justifyContent="space-between" space="3">
-                <Button flex="1" title={"Resetar filtros"} type="tertiary" />
-                <Button flex="1" title={"Aplicar filtros"} />
+                <Button
+                  flex="1"
+                  title={"Resetar filtros"}
+                  type="tertiary"
+                  onPress={handleClearFilter}
+                />
+                <Button
+                  flex="1"
+                  title={"Aplicar filtros"}
+                  onPress={handleFilterProducts}
+                />
               </HStack>
             </Box>
           </Actionsheet.Content>
