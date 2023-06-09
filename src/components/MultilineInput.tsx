0ff88d8bdbee4fd4fa76d71d9fd0e15e4eee5c11
lucas-linard <lucas.linard@outlook.com>
2023-06-09 15:39:12 -0300

    feat: Implemented a error message on MultilineInput

--- src/components/MultilineInput.tsx
+++ src/components/MultilineInput.tsx
@@ -1,30 +1,38 @@
-import {
-  TextArea,
-  ITextAreaProps,
-} from "native-base";
+import { TextArea, FormControl, ITextAreaProps } from "native-base";
 
+type MultilineInputProps = ITextAreaProps & {
+  errorMessage?: string | null;
+};
 
-export function MultilineInput({ ...rest }: ITextAreaProps) {
+export function MultilineInput({
+  errorMessage,
+  isInvalid,
+  ...rest
+}: MultilineInputProps) {
+  const invalid = !!errorMessage || isInvalid;
   return (
-    <TextArea
-    totalLines={8}
-    autoCompleteType={"off"}
-      h="40"
-      px="4"
-      py="3"
-      mb="4"
-      rounded="sm"
-      borderWidth="0"
-      fontFamily={"body"}
-      fontSize="md"
-      bgColor="white"
-      color={"gray.600"}
-      placeholderTextColor={"gray.400"}
-      _focus={{
-        borderWidth: "1",
-        borderColor: "gray.500",
-      }}
-      {...rest}
-    />
+    <FormControl isInvalid={invalid} mb="4">
+      <TextArea
+        totalLines={8}
+        autoCompleteType={"off"}
+        h="40"
+        px="4"
+        py="3"
+        mb="4"
+        rounded="sm"
+        borderWidth="0"
+        fontFamily={"body"}
+        fontSize="md"
+        bgColor="white"
+        color={"gray.600"}
+        placeholderTextColor={"gray.400"}
+        _focus={{
+          borderWidth: "1",
+          borderColor: "gray.500",
+        }}
+        {...rest}
+      />
+      <FormControl.ErrorMessage>{errorMessage}</FormControl.ErrorMessage>
+    </FormControl>
   );
 }
