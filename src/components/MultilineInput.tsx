43051f99bbe5e8a29ab1f4469622f323ff163310
LucasAlencar <lucas.linard@outlook.com>
2023-07-17 17:36:37 -0300

    feat: Added MultilineInput error message

--- src/components/MultilineInput.tsx
+++ src/components/MultilineInput.tsx
@@ -1,18 +1,25 @@
 import {
   TextArea,
   ITextAreaProps,
+  FormControl,
 } from "native-base";
 
 
-export function MultilineInput({ ...rest }: ITextAreaProps) {
+type TextAreaInputProps = ITextAreaProps & {
+  errorMessage?: string | null;
+}
+
+export function MultilineInput({ errorMessage, isInvalid, ...rest }: TextAreaInputProps) {
+  const invalid = !!errorMessage || isInvalid;
+
   return (
+    <FormControl isInvalid={invalid} mb="4">
     <TextArea
     totalLines={8}
     autoCompleteType={"off"}
       h="40"
       px="4"
       py="3"
-      mb="4"
       rounded="sm"
       borderWidth="0"
       fontFamily={"body"}
@@ -26,5 +33,7 @@ export function MultilineInput({ ...rest }: ITextAreaProps) {
       }}
       {...rest}
     />
+    <FormControl.ErrorMessage>{errorMessage}</FormControl.ErrorMessage>
+    </FormControl>
   );
 }
